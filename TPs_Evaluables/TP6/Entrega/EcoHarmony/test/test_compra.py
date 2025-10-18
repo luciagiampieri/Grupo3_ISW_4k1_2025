adf0a9d069f2f456f4ab0a3634d25205786f1a83
Candela Paez <95256@sistemas.frc.utn.edu.ar>
2025-10-18 15:15:03 -0300

    Subiendo Test fecha_valida()

--- TPs_Evaluables/TP6/Entrega/EcoHarmony/test/test_compra.py
+++ TPs_Evaluables/TP6/Entrega/EcoHarmony/test/test_compra.py
@@ -1,6 +1,9 @@
+from datetime import date, timedelta
 from backend.utils import (
-      validarCantidadEntradas
+      validarCantidadEntradas,
+      validarFecha
 )
+
 
 class TestCompra:
 
@@ -23,21 +26,38 @@ class TestCompra:
             print("✅ Test validar cantidad pasó correctamente.")
 
 
-def test_validar_cantidad_invalida(self): #en esta poner todos los casos invalidos, porque estamos probando lo mismo 
+      def test_validar_cantidad_invalida(self): #en esta poner todos los casos invalidos, porque estamos probando lo mismo 
+                  # --- Precondiciones ---
+                  cantidad1 = 0 # cantidad inv
+                  cantidad2 = -2 # cantidad invalida negativa
+                  cantidad3 = 11 # cantidad invalida mayor a 10
+
+                  # --- Pasos del caso de prueba ---
+                  resultado1 = validarCantidadEntradas(cantidad1)
+                  resultado2 = validarCantidadEntradas(cantidad2)
+                  resultado3 = validarCantidadEntradas(cantidad3)
+
+                  # --- Resultados esperados ---
+                  assert resultado1 == False
+                  assert resultado2 == False
+                  assert resultado3 == False
+
+                  # --- Mensaje final ---
+                  print("✅ Test validar cantidad inválida pasó correctamente.")
+
+
+      def test_fecha_valida():
             # --- Precondiciones ---
-            cantidad1 = 0 # cantidad inv
-            cantidad2 = -2 # cantidad invalida negativa
-            cantidad3 = 11 # cantidad invalida mayor a 10
+            fecha = date.today()                # VERIFICAR SI HAY CUPOS DE ENTRADAS AL PARQUE POR DÍA
+            fecha_futuro = date(2025, 11, 15)   # Fecha futura válida
 
             # --- Pasos del caso de prueba ---
-            resultado1 = validarCantidadEntradas(cantidad1)
-            resultado2 = validarCantidadEntradas(cantidad2)
-            resultado3 = validarCantidadEntradas(cantidad3)
+            resultado = validarFecha(fecha)
+            resultado2 = validarFecha(fecha_futuro)
 
             # --- Resultados esperados ---
-            assert resultado1 == False
+            assert resultado == True
             assert resultado2 == False
-            assert resultado3 == False
 
             # --- Mensaje final ---
-            print("✅ Test validar cantidad inválida pasó correctamente.")
+            print("✅ Test fecha válida pasó correctamente.")
